92d3e28d9cdfd4028342baeb7ba7ea9fd1689a79
Dmytro Myhal <dimamygal0708@gmail.com>
2023-09-24 19:59:09 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,57 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    /* Common rules */
+
+    // Avoid using extensions in import e.g. "import Component from './component.tsx'"
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    // Ignore default export requirement
+    'import/prefer-default-export': 'off',
+
+    /* Typescript rules */
+
+    // Allow to use exclamation point from typescript
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    /* React rules */
+
+    // Boolean props start with "is" or "has" e.g. "isActivated" or "hasBorder"
+    'react/boolean-prop-naming': ['error', { validateNested: true }],
+
+    // Prefer destructing for props state or context
+    'react/destructuring-assignment': [
+      'error',
+      'always',
+      { destructureInSignature: 'always' },
+    ],
+
+    // Use arrow functions to declare components
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function' },
+    ],
+  },
+};
